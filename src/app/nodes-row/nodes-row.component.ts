3f309eed6affa1331bbf4af7734c97f75c521158
Diego Cantelli <diegocantelli19@gmail.com>
2020-12-24 18:29:39 -0300

    Usando changeDetection onpush, para atualizar a view somente quando os valores de input forem atualizados

--- src/app/nodes-row/nodes-row.component.ts
+++ src/app/nodes-row/nodes-row.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: '[app-nodes-row]',
   templateUrl: './nodes-row.component.html',
-  styleUrls: ['./nodes-row.component.css']
+  styleUrls: ['./nodes-row.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NodesRowComponent implements OnInit {
 
