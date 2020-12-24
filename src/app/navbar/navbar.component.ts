73b248551bd92d9d858dc208efb72945787952f6
Diego Cantelli <diegocantelli19@gmail.com>
2020-12-24 19:04:38 -0300

    Usando output property para comunicação do componente filho para o componente pai

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { ResourceLoader } from '@angular/compiler';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  @Output() onRefresh: EventEmitter<null> = new EventEmitter<null>();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // quando for clicado no botão reload, a output property irá avisar
+  // o componente pai(appComponent), que por sua vez irá executar o comando associado
+  refresh() {
+    //trecho de código responsável por passar/comunicar para o componente pai mudança no valor
+    this.onRefresh.emit();
   }
 
 }
