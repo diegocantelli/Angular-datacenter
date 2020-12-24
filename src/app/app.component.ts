095710df478c2dcd1bf8c1a0793a280347eb1a8d
Diego Cantelli <diegocantelli19@gmail.com>
2020-12-24 19:19:40 -0300

    Acessando métodos do componente filho através de ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // viewchild permite que um componente seja acessado através do controller
+  //viewchild só permite selecionar os filhos diretos, nesse caso os filhos diretos de app.component.html
+  // enquanto que ao usar template variable permite apenas o acesso do componente através de uma chamada na view
+  @ViewChild(DashboardComponent) dashboard: DashboardComponent;
   title = 'app';
+
+  refresh() {
+    this.dashboard.generateData();
+  }
 }
