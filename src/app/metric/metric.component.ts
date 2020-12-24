0d4a4221e71c0c2b48d6ac4c08dbe4b6c3943869
Diego Cantelli <diegocantelli19@gmail.com>
2020-12-24 18:39:18 -0300

    Usando o lifeCycle hook onChanges e simple changes para detectar mudanças nos inputs do componente

--- src/app/metric/metric.component.ts
+++ src/app/metric/metric.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-metric',
@@ -6,30 +6,38 @@ import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core
   styleUrls: ['./metric.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MetricComponent implements OnInit {
+export class MetricComponent implements OnInit, OnChanges {
 
   private _value: number = 0;
   private _max: number = 100;
 
-  @Input('available')
-  set max(max: number) {
-    if (isNaN(max)) max = 100;
-    this._max = max;
-  }
+  @Input('used') value: number = 0;
+  @Input('available') max: number = 100;
+  // set max(max: number) {
+  //   if (isNaN(max)) max = 100;
+  //   this._max = max;
+  // }
 
-  @Input('used')
-  set value(value: number) {
-    if (isNaN(value)) value = 0;
-    this._value = value;
-  }
 
-  get value(): number { return this._value; }
+  // set value(value: number) {
+  //   if (isNaN(value)) value = 0;
+  //   this._value = value;
+  // }
 
-  get max(): number { return this._max; }
+  // get value(): number { return this._value; }
+
+  // get max(): number { return this._max; }
 
   constructor() { }
 
+
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //o nome das propriedades que mudam de valor ficam dentro de "changes"
+    if (changes.value && isNaN(changes.value.currentValue)) this.value = 0;
+    if (changes.max && isNaN(changes.max.currentValue)) this.max = 0;
   }
 
   isDanger() {
